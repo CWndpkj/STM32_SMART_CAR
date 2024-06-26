dc9665797f3f5b32fb7b23567733472e2e619ae2
[pkjinfinity] <[pkjinfinity@outlook.com]>
2024-06-26 19:29:48 +0800

    add line patrol function

---
+++ Core/Inc/control.h
@@ -0,0 +1,10 @@
+//
+// Created by PKJ on 2024/6/25.
+//
+
+#ifndef CONTROL_H
+#define CONTROL_H
+
+//TODO:小车避障及巡线功能的实现
+
+#endif //CONTROL_H
---
+++ Core/Inc/pid.h
@@ -0,0 +1,8 @@
+//
+// Created by PKJ on 2024/6/25.
+//
+
+#ifndef PID_H
+#define PID_H
+
+#endif //PID_H
